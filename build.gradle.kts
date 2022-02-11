848ff8f77490857c1a7e586136886edb68a7bcae
Jeff Fang <jeff.fang@airwallex.com>
2022-02-11 20:01:50 +0800

    bump version to 1.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.airwallex.grpc-spring") version "1.1.5-SNAPSHOT"
+    id("com.airwallex.grpc-spring") version "1.1.5"
 }
 
 group = "demo"
